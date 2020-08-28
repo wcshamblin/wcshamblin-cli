0de632f0afdb9e03cbe3e9d8c9501185bb76f7e4
wcshamblin <will@shamblin.org>
2020-08-28 15:13:39 -0400

    Initial commit

---
+++ wcshamblin-cli.py
@@ -0,0 +1,74 @@
+from os import system, name
+menu = [
+"""\u001b[0m\u001b[7mWebsite\u001b[0m
+
+Stuff
+
+More stuff
+
+Other stuff
+
+Source Code
+                    🠕 🠗 to navigate, q or e to exit
+                           enter to execute
+""",
+"""Website
+
+\u001b[0m\u001b[7mStuff\u001b[0m
+
+More stuff
+
+Other stuff
+
+Source Code
+                    🠕 🠗 to navigate, q or e to exit
+                           enter to execute
+""",
+]
+
+def clear():
+    system('cls' if name == 'nt' else 'clear')
+
+def get_getch():
+    try:
+        import termios
+    except ImportError as error:
+        # Not POSIX
+        import msvcrt
+        return msvcrt.getch
+    import sys, tty
+    # POSIX
+    def getch():
+        fd = sys.stdin.fileno()
+        old_settings = termios.tcgetattr(fd)
+        try:
+            tty.setraw(fd)
+            ch = sys.stdin.read(1)
+        finally:
+            termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
+        return ch
+    return getch
+getch = get_getch()
+clear()
+i=0
+print(menu[i])
+
+while 1:
+    ch = getch()
+    if ch == "\x1b": # ANSI escape
+        ch = getch()
+        if ch == "[": # Func call
+            ch = getch()
+            if ch == "A" and i>0: # Up arrow
+                i-=1
+                clear()
+                print(menu[i])
+            if ch == "B" and i<len(menu)-1: # Down arrow
+                i+=1
+                clear()
+                print(menu[i])
+
+    if ch == "\r":
+        print("Selected")
+    if ch == "q" or ch == "e":
+        exit()
